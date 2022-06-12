608d95521cad40535eaffa7b6333028cd1a0e1b2
Madeline Sabino <93164849+MADGRISMAD@users.noreply.github.com>
2022-06-12 15:22:08 -0700

    Add files via upload

---
+++ POO/co16-1-6/co16-1-6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //input output
+
+namespace co15_1_6
+{
+    public class Archivo
+    {
+        public void EscribeDoc()
+        {
+            //nombre del archivo fisico o real
+            string nombreArchivo = "temp.txt";
+            FileStream archivo;//nombre logico del archivo
+            //este nombre logico se utiliza en el programa para acceder a los datos del archivo
+            archivo = new FileStream(nombreArchivo, FileMode.OpenOrCreate, FileAccess.Write);
+            //abre el archivo y lo crea si no existe. el acceso al mismo sera para escribir datos
+            StreamWriter escribirArchivo = new StreamWriter(archivo);
+            escribirArchivo.WriteLine("mi primer renglon");
+            escribirArchivo.WriteLine("mi primer renglon");
+            escribirArchivo.WriteLine("mi primer renglon");
+            escribirArchivo.WriteLine("mi primer renglon");
+            escribirArchivo.WriteLine("mi primer renglon");
+            escribirArchivo.WriteLine("mi primer renglon");
+            escribirArchivo.Close();
+        }
+        public void Leer()
+        {
+            string nombreArchivo = "temp.txt";
+            FileStream archivo;
+            archivo = new FileStream(nombreArchivo, FileMode.Open, FileAccess.Read);
+            StreamReader leyendo = new StreamReader(archivo);
+            while (leyendo.Peek() > -1) //-1 oidica fin del archivo
+                Console.WriteLine(leyendo.ReadLine());
+            leyendo.Close();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Archivo archivo = new Archivo();
+            archivo.EscribeDoc();
+            archivo.Leer();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ POO/examen/examen/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public class Edades
+    {
+        int edad;
+        int año;
+        public void Captura()
+        {
+            try
+            {
+                Console.WriteLine("Ingrese año de nacimiento");
+                año = int.Parse(Console.ReadLine());
+                edad = 2021 - año;
+                if (edad >= 18 && edad <= 65)
+                {
+                    Console.WriteLine("Tu edad es: {0}", edad);
+                    Console.WriteLine("Puedes continuar trabajando");
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Tu edad es: {0}", edad);
+                Console.WriteLine("No puedes trabajar si no tienes entre 18 y 65 años");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Edades edad = new Edades();
+            edad.Captura();
+            Console.ReadKey();
+        }
+    }
+}
